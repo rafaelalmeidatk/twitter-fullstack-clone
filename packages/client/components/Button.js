6e328c6cee348a82f4a59acece21f909d9c0e45d
Rafael Almeida <rafaelalmeidatk@gmail.com>
2019-04-16 05:15:04 -0300

    Add better styling on Button

--- packages/client/components/Button.js
+++ packages/client/components/Button.js
@@ -22,16 +22,40 @@ const Button = ({
           background: #fff;
           border: 1px solid #1da1f2;
           border-radius: 50px;
+          cursor: pointer;
+          outline: none;
 
           font-size: 0.9em;
           font-weight: bold;
           padding: 12px;
+
+          transition: background 0.2s ease;
+        }
+
+        .btn:hover {
+          background: #e9f5fd;
+        }
+
+        .btn:active {
+          background: #dcf1ff;
+        }
+
+        .btn:focus:not(:active) {
+          box-shadow: 0 0 0 0.18em rgb(154, 210, 249);
         }
 
         .btn.primary {
           color: #fff;
           background: #1da1f2;
-          border: 1px solid #fff;
+          border: none;
+        }
+
+        .btn.primary:hover {
+          background: #1899e8;
+        }
+
+        .btn.primary:active {
+          background: #148dd8;
         }
 
         .btn.full {
