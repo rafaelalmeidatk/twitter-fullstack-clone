6aef3f7b61245bb6a21eba782be3f999c82e60ea
Rafael Almeida <rafaelalmeidatk@gmail.com>
2019-04-30 01:35:21 -0300

    Create a lot of seed data for development

---
+++ packages/server/src/db/seeds/development/02_tweets.js
@@ -0,0 +1,79 @@
+const faker = require('faker');
+const usersId = require('./01_users').ids;
+
+const createRandomTweets = () => {
+  let tweets = [];
+
+  // Normal tweets
+  Object.keys(usersId).forEach(key => {
+    const userId = usersId[key];
+    const n = faker.random.number({ min: 7, max: 15 });
+
+    for (let i = 0; i < n; i++) {
+      tweets.push({
+        id: faker.random.uuid(),
+        userId,
+        content: faker.lorem.sentence(faker.random.number({ min: 5, max: 18 })),
+        created_at: faker.date.recent(20),
+      });
+    }
+  });
+
+  // Retweets
+  tweets.forEach(tweet => {
+    if (!tweet.content) return;
+
+    Object.keys(usersId).forEach(key => {
+      const userId = usersId[key];
+
+      if (Math.random() > 0.2) return; // 20% chance
+      tweets.push({
+        id: faker.random.uuid(),
+        userId,
+        created_at: faker.date.between(tweet.created_at, new Date()),
+        retweetForTweetId: tweet.id,
+      });
+    });
+  });
+
+  // Likes
+  tweets.forEach(tweet => {
+    if (!tweet.content) return;
+
+    Object.keys(usersId).forEach(key => {
+      const userId = usersId[key];
+
+      if (Math.random() > 0.5) return; // 50% chance
+      tweets.push({
+        id: faker.random.uuid(),
+        userId,
+        created_at: faker.date.between(tweet.created_at, new Date()),
+        likeForTweetId: tweet.id,
+      });
+    });
+  });
+
+  return tweets;
+};
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('tweets')
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex('tweets').insert([
+        {
+          userId: usersId.steve,
+          content: 'Hello World',
+          created_at: faker.date.recent(20),
+        },
+        {
+          userId: usersId.steve,
+          content: 'React is awesome!',
+          created_at: faker.date.recent(20),
+        },
+        ...createRandomTweets(),
+      ]);
+    });
+};
---
+++ packages/server/src/db/seeds/development/03_follows.js
@@ -0,0 +1,32 @@
+const usersId = require('./01_users').ids;
+
+const createRandomFollows = () => {
+  let follows = [];
+
+  Object.keys(usersId).forEach(key1 => {
+    const userId = usersId[key1];
+
+    Object.keys(usersId).forEach(key2 => {
+      const targetUserId = usersId[key2];
+
+      if (userId !== targetUserId && Math.random() > 0.6) {
+        follows.push({
+          userId,
+          followingId: targetUserId,
+        });
+      }
+    });
+  });
+
+  return follows;
+};
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('follows')
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex('follows').insert(createRandomFollows());
+    });
+};
